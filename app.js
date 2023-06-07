f067906512e725cef773ff32c87b2f79727ef1b7
minsik <minsik@minsiks-MacBook-Pro.local>
2023-06-07 13:30:28 +0900

    INITIAL SETTING

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// const routes = require("./src/routes");
+
+const createApp = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(morgan("tiny"));
+  app.use(express.json());
+  // app.use(routes);
+
+  app.get("/ping", (req, res) => {
+    res.status(200).json({ message : "pong?" });
+  })
+
+  return app;
+};
+
+module.exports = { createApp };
